3eb3c60a127e5072113f3e63d87c7c11a4d7d5c0
d_lasinda <d.lasinda@directfn.com>
2024-05-31 14:36:49 +0530

    12

--- SendEmail_Function/Function1.cs
+++ SendEmail_Function/Function1.cs
@@ -17,6 +17,7 @@ namespace SendEmail_Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+           
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
